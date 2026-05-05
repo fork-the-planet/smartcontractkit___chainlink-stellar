0dea3af0a1ca2d2b45bf5bdadcac7a7754975ce3
faisal-link <faisal.altameemi@smartcontract.com>
2026-05-05 22:00:36 +0400

    update service cmd entrypoints

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -1,3 +1,10 @@
+// Stellar executor binary entry point.
+//
+// All per-chain wiring (config loading, OffRamp/RMN address resolution, keystore-backed
+// transmitter construction) lives in ccv/accessors. The chainlink-ccv executor service
+// pulls DestinationReader and ContractTransmitter directly off the Accessor returned by
+// the registry, so this file only needs to declare the bootstrapper-managed keys this
+// service requires.
 package main
 
 import (
@@ -6,20 +13,20 @@ import (
 	_ "github.com/lib/pq"
 	"go.uber.org/zap/zapcore"
 
-	chainsel "github.com/smartcontractkit/chain-selectors"
 	"github.com/smartcontractkit/chainlink-ccv/bootstrap"
 	executorcmd "github.com/smartcontractkit/chainlink-ccv/cmd/executor"
-	sourcereader "github.com/smartcontractkit/chainlink-stellar/ccv/source_reader"
+	"github.com/smartcontractkit/chainlink-common/keystore"
+
+	_ "github.com/smartcontractkit/chainlink-stellar/ccv/accessors" // registers Stellar chainaccess constructor
+	"github.com/smartcontractkit/chainlink-stellar/ccv/common"
 )
 
 func main() {
 	if err := bootstrap.Run(
 		"StellarExecutor",
-		executorcmd.NewServiceFactory[sourcereader.ReaderConfig](
-			chainsel.FamilyStellar,
-			CreateStellarExecutorComponents,
-		),
+		executorcmd.NewFactory(),
 		bootstrap.WithLogLevel(zapcore.InfoLevel),
+		bootstrap.WithKey(common.StellarTransmitterKeyName, "transmitting", keystore.Ed25519),
 	); err != nil {
 		panic(fmt.Sprintf("failed to run Stellar executor: %s", err.Error()))
 	}
