8a7aa9b5f482232e708a54a9de06051ef17a4e59
faisal-link <faisal.altameemi@smartcontract.com>
2026-05-05 22:34:28 +0400

    update stellar executor entrypoint to provide default key name

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -5,6 +5,11 @@
 // pulls DestinationReader and ContractTransmitter directly off the Accessor returned by
 // the registry, so this file only needs to declare the bootstrapper-managed keys this
 // service requires.
+//
+// chainlink-ccv devenv always POSTs GetKeys for executor.DefaultEVMTransmitterKeyName
+// after the container starts (see build/devenv/services/executor/base.go). That ECDSA key
+// is not used for Soroban submission but must exist so bootstrap HTTP does not return 500.
+// Soroban signing uses common.StellarTransmitterKeyName (Ed25519).
 package main
 
 import (
@@ -15,6 +20,7 @@ import (
 
 	"github.com/smartcontractkit/chainlink-ccv/bootstrap"
 	executorcmd "github.com/smartcontractkit/chainlink-ccv/cmd/executor"
+	"github.com/smartcontractkit/chainlink-ccv/executor"
 	"github.com/smartcontractkit/chainlink-common/keystore"
 
 	_ "github.com/smartcontractkit/chainlink-stellar/ccv/accessors" // registers Stellar chainaccess constructor
@@ -26,6 +32,7 @@ func main() {
 		"StellarExecutor",
 		executorcmd.NewFactory(),
 		bootstrap.WithLogLevel(zapcore.InfoLevel),
+		bootstrap.WithKey(executor.DefaultEVMTransmitterKeyName, "transmitting", keystore.ECDSA_S256),
 		bootstrap.WithKey(common.StellarTransmitterKeyName, "transmitting", keystore.Ed25519),
 	); err != nil {
 		panic(fmt.Sprintf("failed to run Stellar executor: %s", err.Error()))
